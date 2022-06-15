314a7cafda6839b8feeb1611d30206b1c23a4b86
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-15 14:49:47 +0200

    shows coordinates on mouseclick and switches display to the left when too close to border

--- sketch.js
+++ sketch.js
@@ -4,4 +4,16 @@ function setup() {
 
 function draw() {
   background(220);
+  if (mouseIsPressed) {
+    showCoordinates();
+  }
+}
+function showCoordinates(x, y) {
+  let coordinates = mouseX + "," + mouseY;
+
+  if (mouseX > 350) {
+    text(coordinates, mouseX - 50, mouseY);
+  } else {
+    text(coordinates, mouseX, mouseY);
+  }
 }
